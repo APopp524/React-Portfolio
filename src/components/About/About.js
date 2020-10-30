c7d3ff4c38ac617ba198c7884a14be2b607be312
Andrew Popp <apopp524@gmail.com>
2020-10-30 16:44:38 -0500

    updated about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,10 +21,10 @@ export const About = () => {
                 </h1>
                 
                 <p class="p-first text-white">
-                 Full Stack Web Developer with a Digital Media Production degree and experience in the live events, media, and television industry. Recently earned a certificate in Full Stack development from the University of Kansas with new skills in Javascript, CSS, HTML, and responsive web design.
+                Full Stack Web Developer with a Digital Media Production degree and experience in the live events, media, and television industry. Recently earned a certificate in Full Stack development from the University of Kansas with new skills in JavaScript, jQuery, CSS3, HTML5, MySQL, NoSQL, Node.js, React.js and responsive web design.
                 </p>
                 <p class="text-white">
-                 I enjoy problem-solving and am a flexible worker that can work as a team or individually to get the task completed. I am passionate about web design and computer science and strive to further my knowledge in any way I can, and I look forward to challenging myself every day. 
+                I am a self-motivated problem solver that is flexible working with a team or individually and am always striving to learn and find new creative challenges everyday.
                 </p>
                 <a href={Resume} class="btn btn-main">Resume PDF</a>
              
