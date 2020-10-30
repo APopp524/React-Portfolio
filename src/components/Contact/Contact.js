a57f54414d050c539dcd10408e270b491cd90660
Andrew Popp <apopp524@gmail.com>
2020-10-30 15:22:40 -0500

    updated contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,13 +6,23 @@ export const Contact = () => {
         
 
 <div align="center">
-    <h2>Let's Talk!</h2>
-    <form id="form" class="topBefore" action="mailto:apopp524@gmail.com" method="post" enctype="text/plain">		
-        <input id="name" type="text" placeholder="NAME"></input>
-        <input id="email" type="text" placeholder="E-MAIL"></input>
-        <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
-        <input id="submit" type="submit" value="SUBMIT"></input>
-    </form>
+    <h1>Let's Talk!</h1>
+    <ul id="list">
+      <li id="icons">
+        <i class="fa fa-envelope"></i>
+        <a>apopp524@gmail.com</a>
+      </li>
+      <li>
+        <i class="fa fa-phone"></i>
+        <a>314-374-5558</a>
+      </li>
+      </ul>
+            <form id="form" class="topBefore" action="mailto:apopp524@gmail.com" method="post" enctype="text/plain">		
+                <input id="name" type="text" placeholder="NAME"></input>
+                <input id="email" type="text" placeholder="E-MAIL"></input>
+                <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+                <input id="submit" type="submit" value="SUBMIT"></input>
+            </form>
 </div>
 
         );
